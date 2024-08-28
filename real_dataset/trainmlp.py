637b4a3fcec8ae23dfd95c52ebbee55dcaea9d2d
motion <vallabhnadgir@gmail.com>
2024-08-27 19:17:13 -0500

    replace numpy operations with torch

--- real_dataset/trainmlp.py
+++ real_dataset/trainmlp.py
@@ -35,115 +35,100 @@ wandb_logger = WandbLogger(log_model="all")
 wandb.init(project=f"loss_{args.loss}_+mece-fusion_na-real_dataset")
 
 class Calibration_calc_3D:
-    def __init__(self, tiers =np.arange(11)/10,no_void = False,one_hot = True):
+    def __init__(self, tiers=torch.arange(11) / 10, no_void=False, one_hot=True):
         self.tiers = tiers
-        self.total_bin_members = np.zeros(len(tiers)-1)
-        self.correct_bin_members = np.zeros(len(tiers)-1)
-        self.total_bin_confidence = np.zeros(len(tiers)-1)
+        self.total_bin_members = torch.zeros(len(tiers) - 1, device=device)
+        self.correct_bin_members = torch.zeros(len(tiers) - 1, device=device)
+        self.total_bin_confidence = torch.zeros(len(tiers) - 1, device=device)
         self.no_void = no_void
         self.one_hot = one_hot
-    def update_bins(self,semantic_label,semantic_label_gt):
-        if(self.no_void):
-            if(self.one_hot):
-                gt_labels = semantic_label_gt.argmax(axis=1)
+
+    def update_bins(self, semantic_label, semantic_label_gt):
+        if self.no_void:
+            if self.one_hot:
+                gt_labels = torch.argmax(semantic_label_gt, dim=1)
             else:
                 gt_labels = semantic_label_gt
-            semantic_label_gt = semantic_label_gt[gt_labels != 0]
-            semantic_label = semantic_label[gt_labels!=0]
-        max_conf = semantic_label.max(axis = 1)
-        # total_bin_members = np.zeros(len(self.tiers)-1)
-        # correct_bin_members = np.zeros(len(self.tiers)-1)
-        pred = semantic_label.argmax(axis =1)
-        if(self.one_hot):
-            comparison_sheet = semantic_label_gt.argmax(axis=1) == pred
+            mask = gt_labels != 0
+            semantic_label_gt = semantic_label_gt[mask]
+            semantic_label = semantic_label[mask]
+
+        max_conf, pred = torch.max(semantic_label, dim=1)
+        if self.one_hot:
+            comparison_sheet = torch.argmax(semantic_label_gt, dim=1) == pred
         else:
             comparison_sheet = semantic_label_gt == pred
-        for i in range(len(self.tiers)-1):
-            if(self.tiers[i+1] != 1.0):
-                conf_mask_tier = np.logical_and(max_conf>=self.tiers[i],max_conf<self.tiers[i+1])
+
+        for i in range(len(self.tiers) - 1):
+            if self.tiers[i + 1] != 1.0:
+                conf_mask_tier = (max_conf >= self.tiers[i]) & (max_conf < self.tiers[i + 1])
             else:
-                conf_mask_tier = np.logical_and(max_conf>=self.tiers[i],max_conf<=self.tiers[i+1])
-#             pdb.set_trace()
-            self.total_bin_members[i] += conf_mask_tier.sum()
-            self.correct_bin_members[i] += comparison_sheet[conf_mask_tier].sum()
-            self.total_bin_confidence[i] += max_conf[conf_mask_tier].sum()
+                conf_mask_tier = (max_conf >= self.tiers[i]) & (max_conf <= self.tiers[i + 1])
+
+            self.total_bin_members[i] += conf_mask_tier.sum().float()
+            self.correct_bin_members[i] += comparison_sheet[conf_mask_tier].sum().float()
+            self.total_bin_confidence[i] += max_conf[conf_mask_tier].sum().float()
+
     def return_calibration_results(self):
-        return self.correct_bin_members/self.total_bin_members,self.total_bin_confidence/self.total_bin_members,self.tiers[1:]
-    
+        acc = self.correct_bin_members / self.total_bin_members
+        conf = self.total_bin_confidence / self.total_bin_members
+        return acc, conf, self.tiers[1:]
+
     def get_ECE(self):
-        if(np.all(self.total_bin_members == 0)):
-            return np.nan
+        if torch.all(self.total_bin_members == 0):
+            return torch.tensor(float('nan'), device=device)
         else:
-            acc = self.correct_bin_members/self.total_bin_members
-            conf = self.total_bin_confidence/self.total_bin_members
-            
-            share = np.nan_to_num(((self.total_bin_members)/(self.total_bin_members.sum())),nan=0)
-            # print(share,np.abs(acc-conf))
-            return (share*np.nan_to_num(np.abs(acc-conf),nan = 0)).sum()
+            acc, conf = self.return_calibration_results()[:2]
+            share = torch.nan_to_num(self.total_bin_members / self.total_bin_members.sum(), nan=0.0)
+            return (share * torch.nan_to_num(torch.abs(acc - conf), nan=0.0)).sum()
+
 
 class mECE_Calibration_calc_3D:
-    def __init__(self, tiers =np.arange(11)/10,no_void = False,one_hot = True,n_classes = 21):
+    def __init__(self, tiers=torch.arange(11) / 10, no_void=False, one_hot=True, n_classes=21):
         self.tiers = tiers
         self.no_void = no_void
         self.one_hot = one_hot
         self.n_classes = n_classes
         self.cals = {}
-        self.agg_cal = Calibration_calc_3D(tiers = self.tiers,no_void = self.no_void,one_hot = self.one_hot)
+        self.agg_cal = Calibration_calc_3D(tiers=self.tiers, no_void=self.no_void, one_hot=self.one_hot)
         for i in range(self.n_classes):
-            self.cals.update({i:Calibration_calc_3D(self.tiers,self.no_void,self.one_hot)})
-    def update_bins(self,semantic_label,semantic_label_gt):
-        if(self.one_hot):
-            map_gt = semantic_label_gt.argmax(axis = 1)
+            self.cals.update({i: Calibration_calc_3D(self.tiers, self.no_void, self.one_hot)})
+
+    def update_bins(self, semantic_label, semantic_label_gt):
+        if self.one_hot:
+            map_gt = torch.argmax(semantic_label_gt, dim=1)
         else:
             map_gt = semantic_label_gt
-        self.agg_cal.update_bins(semantic_label,semantic_label_gt)
+
+        self.agg_cal.update_bins(semantic_label, semantic_label_gt)
         for i in range(self.n_classes):
             mask = map_gt == i
-            if(map_gt[mask].shape[0]>0):
-                self.cals[i].update_bins(semantic_label[mask],semantic_label_gt[mask])
+            if map_gt[mask].shape[0] > 0:
+                self.cals[i].update_bins(semantic_label[mask], semantic_label_gt[mask])
+
     def return_calibration_results(self):
         results = {}
         for i in range(self.n_classes):
-            results.update({i:self.cals[i].return_calibration_results()})
-        results.update({'aggregate':self.agg_cal.return_calibration_results()})
+            results.update({i: self.cals[i].return_calibration_results()})
+        results.update({'aggregate': self.agg_cal.return_calibration_results()})
         return results
+
     def get_ECEs(self):
         results = []
         for i in range(self.n_classes):
             results.append(self.cals[i].get_ECE())
         results.append(self.agg_cal.get_ECE())
-        # results.append(self.get_TL_ECE())
-        return results 
+        return results
+
     def get_mECE(self):
         ECEs = []
         for i in range(self.n_classes):
-            if(i !=0):
+            if i != 0:
                 ECEs.append(self.cals[i].get_ECE())
-        ECEs = np.array(ECEs)
-        #filtering out pesky nans due to bad calibrations that end up with no predictions in the fixed case and penalizing those cases
-        ECEs[np.logical_not(np.isfinite(ECEs))] = 1.0
-        return np.mean(ECEs)
-    def get_TL_ECE(self):
-        accuracies = []
-        confidences = []
-        memberships = []
-        for i in range(self.n_classes):
-            acc,conf,borders = self.cals[i].return_calibration_results()
-            membership = self.cals[i].total_bin_members
-            accuracies.append(acc)
-            confidences.append(conf)
-            memberships.append(membership)
-        accuracies = np.array(accuracies)
-        confidences = np.array(confidences)
-        memberships = np.array(memberships)
-        bin_membership_totals = memberships.sum(axis =0,keepdims = True)
-        within_bin_fractions = np.nan_to_num(memberships/bin_membership_totals,nan = 0,posinf = 0,neginf = 0)
-        differences = np.nan_to_num(np.abs(accuracies-confidences),nan = 0,posinf = 0,neginf = 0)
-        mean_bin_differences = (differences*within_bin_fractions).sum(axis = 0)
-        bin_fractions = bin_membership_totals/bin_membership_totals.sum()
-        weighted_delta_bs = np.nan_to_num(bin_fractions*mean_bin_differences,nan = 0,posinf = 0,neginf = 0)
-        TL_ECE = weighted_delta_bs.sum()
-        return TL_ECE
+        ECEs = torch.tensor(ECEs, device=device)
+        ECEs[~torch.isfinite(ECEs)] = 1.0
+        return torch.mean(ECEs)
+
 
 # Define the encoder MLP with four layers
 class Encoder(nn.Module):
@@ -272,10 +257,10 @@ class LitAutoEncoder(L.LightningModule):
         # if self.check_for_nan(output_vector_encoded_fusion, 'output_vector_encoded_fusion_log'):
         #     return torch.Tensor([NaN])
         cali = mECE_Calibration_calc_3D(one_hot=False)
-        cali.update_bins((output_vector_encoded_fusion.detach().cpu().numpy()), gt.detach().cpu().numpy())
+        cali.update_bins((output_vector_encoded_fusion), gt)
         mece = cali.get_mECE()
 
-        loss = lossf(output_vector_encoded_fusion, ground_truth) + torch.from_numpy(np.asarray(mece))
+        loss = lossf(output_vector_encoded_fusion, ground_truth) + mece
         self.log("train_loss", loss)
 
         if batch_idx % 2000 == 0:
@@ -329,11 +314,11 @@ class LitAutoEncoder(L.LightningModule):
             output_vector_encoded_fusion = torch.log(output_vector_encoded_fusion)
 
         cali = mECE_Calibration_calc_3D(one_hot=False)
-        cali.update_bins((output_vector_encoded_fusion.detach().cpu().numpy()), gt.detach().cpu().numpy())
+        cali.update_bins((output_vector_encoded_fusion), gt)
         mece = cali.get_mECE()
             
 
-        val_loss = lossf(output_vector_encoded_fusion, ground_truth) + torch.from_numpy(np.asarray(mece))
+        val_loss = lossf(output_vector_encoded_fusion, ground_truth) + mece
         self.log("val_loss", val_loss)
 
     def test_step(self, batch, batch_idx):
@@ -369,11 +354,11 @@ class LitAutoEncoder(L.LightningModule):
             output_vector_encoded_fusion = torch.log(output_vector_encoded_fusion)
 
         cali = mECE_Calibration_calc_3D(one_hot=False)
-        cali.update_bins((output_vector_encoded_fusion.detach().cpu().numpy()), gt.detach().cpu().numpy())
+        cali.update_bins((output_vector_encoded_fusion), gt)
         mece = cali.get_mECE()
             
 
-        test_loss = lossf(output_vector_encoded_fusion, ground_truth) + torch.from_numpy(np.asarray(mece))
+        test_loss = lossf(output_vector_encoded_fusion, ground_truth) + mece
         self.log("test_loss", test_loss)
 
     def configure_optimizers(self):
